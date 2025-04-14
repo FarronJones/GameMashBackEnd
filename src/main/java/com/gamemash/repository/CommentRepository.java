661721f893999ad2fc7a76c3e9c977e3432d5d5e
Farron <jones.f03@mymail.sxu.edu>
2025-04-14 07:22:08 -0500

    Check Discord!

--- src/main/java/com/gamemash/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gamemash.repository;
-
-import com.gamemash.entity.Comments;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface CommentRepository extends JpaRepository<Comments, Long> {
-    List<Comments> findByUserId(Long userId);
-}
